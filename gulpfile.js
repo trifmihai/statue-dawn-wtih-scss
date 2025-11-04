dd25eb8abe216946674698edfa1d6553a25e8602
mike_trif <trifmihai.business@gmail.com>
2025-11-04 21:05:33 +0200

    Third Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task("sass", function () {
-  return gulp.src("styles/*.scss").pipe(sass()).pipe(gulp.dest("assets"));
+gulp.task('sass', function () {
+  return gulp.src('styles/*.scss').pipe(sass()).pipe(gulp.dest('assets'));
 });
 
-gulp.task("watch", function () {
-  gulp.watch("styles/**/*.scss", gulp.series("sass"));
+gulp.task('watch', function () {
+  gulp.watch('styles/**/*.scss', gulp.series('sass'));
 });
